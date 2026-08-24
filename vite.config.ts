5357acc71a005b2d00a643bb69bea1a47df3085f
Vexeriz <josuemorelosx@gmail.com>
2026-08-24 17:43:29 -0500

    Add nitro plugin to Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   resolve: { tsconfigPaths: true },
-  plugins: [tailwindcss(), tanstackStart({ target: "vercel" }), viteReact()],
+  plugins: [tailwindcss(), tanstackStart(), nitro(), viteReact()],
 });
